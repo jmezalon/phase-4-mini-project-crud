459aad4584cbfda2ee735c7226a93caa494becae
Max Mezalon <jmezalon@gmail.com>
2021-12-13 13:32:56 -0500

    commit tree e8830256e888aae780f2f85ee21e6aedc1fc7b52

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,41 @@
+class SpicesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    # GET /spices
+    def index 
+        render json: Spice.all, status: :ok
+    end
+
+    # POST /spices
+    def create 
+        newPlant = Spice.create(spice_params)
+        render json: newPlant, status: :created
+    end
+
+    # PATCH /spices/:id
+    def update 
+        spice = find_spice
+        spice.update(spice_params)
+        render json: spice, status: :accepted
+    end
+
+    # DELETE /spices/:id
+    def destroy
+        spice = find_spice
+        spice.destroy
+        head :no_content
+    end
+
+    private 
+    def render_not_found_response
+        render json: { error: "Spice not found" }, status: :not_found
+    end
+
+    def find_spice 
+        Spice.find(params[:id])
+    end
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+end
